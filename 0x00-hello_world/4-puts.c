f4b46ebc5f7918bb79244fab3bc27d8b21d99ae5
Clshec <clshecapp@gmail.com>
2023-08-18 02:50:16 -0700

    hwo

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+nclude<stdio.h>
+/**
+ * * main - Prints string character wiht puts.
+ * * Return: 0.
+ * */
+
+int main(void)
+{
+		puts("\"Programming is like building a multilingual puzzle");
+			return (0);
+}
